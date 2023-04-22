0fafd9d4688071344a2d37587c290b9d2e7e6316
Gamaliel Garcia <46827955+EGAMAGZ@users.noreply.github.com>
2023-04-22 16:26:25 -0600

    Temporal fix for tabler-icons

---
+++ components/Icons.tsx
@@ -0,0 +1,3 @@
+import IconPlus from "https://deno.land/x/tabler_icons_tsx@0.0.3/tsx/plus.tsx"
+
+export default IconPlus;
--- islands/Input.tsx
+++ islands/Input.tsx
@@ -1,6 +1,8 @@
 import { useSignal } from "@preact/signals";
 import { IS_BROWSER } from "$fresh/runtime.ts";
 import useTaskList from "../hooks/useTaskList.tsx";
+import IconPlus from "../components/Icons.tsx";
+// TODO: Check why IconPlus from table-icons downloads node_modules and launches error.
 
 export default function NewTaskInput() {
   const isValid = useSignal<boolean>(true);
@@ -44,13 +46,13 @@ export default function NewTaskInput() {
           disabled={!IS_BROWSER}
         />
         <button class="inline-flex items-center text-uppercase font-medium">
-          {/* <IconPlus size="18" /> */}
           Add
+          <IconPlus size={24} />
         </button>
       </div>
 
       {!isValid.value && (
-        <div class="bg-red-400 text-white px-4 py-2 italic">
+        <div class="bg-red-400 border-1 border-red-500 text-white px-4 py-2 italic">
           Please enter a title.
         </div>
       )}
